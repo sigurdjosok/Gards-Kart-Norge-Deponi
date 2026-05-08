27c8fe828a55e0232623ca03317a2dc3a23cff92
sigurdjosok <sigurd.jos@gmail.com>
2026-05-08 12:59:11 +0200

    Update NorgeKart.jsx

--- src/NorgeKart.jsx
+++ src/NorgeKart.jsx
@@ -18,26 +18,34 @@ const plantIcon = L.divIcon({
 });
 
 // ✅ HARDKODET TINE ANLEGG (kan utvides senere)
+
 const tinePlants = [
-  { name: "TINE Oslo", lat: 59.94, lon: 10.88 },
+  { name: "TINE Alta", lat: 69.97, lon: 23.29 },
   { name: "TINE Bergen", lat: 60.39, lon: 5.32 },
-  { name: "TINE Trondheim (Tunga)", lat: 63.43, lon: 10.47 },
-  { name: "TINE Verdal", lat: 63.79, lon: 11.47 },
-  { name: "TINE Frya", lat: 61.56, lon: 9.75 },
   { name: "TINE Brumunddal", lat: 60.88, lon: 10.94 },
   { name: "TINE Byrkjelo", lat: 61.89, lon: 6.51 },
-  { name: "TINE Ålesund", lat: 62.47, lon: 6.15 },
-  { name: "TINE Ørsta", lat: 62.20, lon: 6.13 },
+  { name: "TINE Dovre", lat: 61.99, lon: 9.26 },
   { name: "TINE Elnesvågen", lat: 62.86, lon: 7.15 },
-  { name: "TINE Tresfjord", lat: 62.59, lon: 7.25 },
-  { name: "TINE Vik i Sogn", lat: 61.09, lon: 6.57 },
-  { name: "TINE Alta", lat: 69.97, lon: 23.27 },
-  { name: "TINE Sandnessjøen", lat: 66.02, lon: 12.63 },
+  { name: "TINE Frya", lat: 61.56, lon: 9.75 },
+  { name: "TINE Haukeli", lat: 59.76, lon: 7.46 },
+  { name: "TINE Jæren", lat: 58.78, lon: 5.73 },
+  { name: "TINE Kristiansand", lat: 58.16, lon: 8.00 },
+  { name: "TINE Lom & Skjåk", lat: 61.84, lon: 8.57 },
+  { name: "TINE Oslo", lat: 59.94, lon: 10.86 },
+  { name: "TINE Sandnessjøen", lat: 66.02, lon: 12.62 },
+  { name: "TINE Selbu", lat: 63.22, lon: 11.04 },
+  { name: "TINE Setesdal", lat: 58.72, lon: 7.30 },
+  { name: "TINE Sømna", lat: 65.32, lon: 12.17 },
   { name: "TINE Storsteinnes", lat: 69.24, lon: 19.23 },
   { name: "TINE Tana", lat: 70.19, lon: 28.18 },
-  { name: "TINE Selbu", lat: 63.22, lon: 11.04 },
-  { name: "TINE Sømna", lat: 65.32, lon: 12.17 },
+  { name: "TINE Tresfjord", lat: 62.59, lon: 7.25 },
+  { name: "TINE Tunga (Trondheim)", lat: 63.43, lon: 10.47 },
+  { name: "TINE Verdal", lat: 63.79, lon: 11.47 },
+  { name: "TINE Vik i Sogn", lat: 61.09, lon: 6.57 },
+  { name: "TINE Ørsta", lat: 62.20, lon: 6.13 },
+  { name: "TINE Ålesund", lat: 62.47, lon: 6.15 }
 ];
+
 
 export default function NorgeKart() {
   const [items, setItems] = useState([]);
